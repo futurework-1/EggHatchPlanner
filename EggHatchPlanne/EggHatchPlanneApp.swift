7e5bd4c7bdb74673c9195cd37e25c7bd8459413a
Adam <tabievadam@gmail.com>
2025-08-06 16:34:25 +0200

    Start

---
+++ EggHatchPlanne/EggHatchPlanneApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggHatchPlanneApp.swift
+//  EggHatchPlanne
+//
+//  Created by Адам Табиев on 06.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggHatchPlanneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
